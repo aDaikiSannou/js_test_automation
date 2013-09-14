857206cfc79d50cb3f8e124792511c0576763885
Daiki.Sanno <daiki.sanno@gmail.com>
2013-09-14 22:01:45 +0900

    add jasmine tap reporter.

--- test_tool/run_jasmine.sh
+++ test_tool/run_jasmine.sh
@@ -23,7 +23,8 @@ STOP_URL="http://localhost:$PORT/stop"
 if [ -d $PHANTOM_REPORT_DIR ]; then
   rm -r $PHANTOM_REPORT_DIR
 fi
+mkdir --parents $PHANTOM_REPORT_DIR
 java -Dfile.encoding=UTF-8 -jar $JSCOVER_PATH -ws --port=$PORT --document-root=$ROOT_DIR --no-instrument=$LIBS_DIR --no-instrument=$TEST_DIR --report-dir=$REPORT_DIR &
-phantomjs run-jscover-jasmine.js $TEST_URL
+phantomjs run-jscover-jasmine.js $TEST_URL | sed -n -e "s/^###TAP###//gp" > $PHANTOM_REPORT_DIR/tap.txt
 java -cp $JSCOVER_PATH jscover.report.Main --format=COBERTURAXML $PHANTOM_REPORT_DIR $SRC_DIR
 phantomjs stop-jscover.js $STOP_URL
